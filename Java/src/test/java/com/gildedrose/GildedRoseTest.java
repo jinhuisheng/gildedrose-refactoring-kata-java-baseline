a39e69a78a984b14024809f1dade520c0f4924b2
金慧生 <jinhuisheng@wowoshijie.com>
2020-01-13 12:06:52 +0800

    构建安全网---完成测试覆盖率测试数据准备

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,26 @@
 package com.gildedrose;
 
+import org.approvaltests.combinations.CombinationApprovals;
+import org.approvaltests.reporters.JunitReporter;
+import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
     @Test
+    @UseReporter(JunitReporter.class)
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        CombinationApprovals.verifyAllCombinations(this::oneDayPass
+                , new String[]{"foo","Aged Brie","Backstage passes to a TAFKAL80ETC concert","Sulfuras, Hand of Ragnaros"}
+                , new Integer[]{-1,0,5,6,11}
+                , new Integer[]{0,1,49,50,51});
+    }
+
+    private String oneDayPass(String name, int sellIn, int quality) {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        return app.items[0].toString();
     }
 
 }
